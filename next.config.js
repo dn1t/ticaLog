2bff27f045d924be720b6e88a368a150d09b9af4
thoratica <me@tica.fun>
2021-11-06 23:16:38 +0900

    fixed next.config.js for production

--- next.config.js
+++ next.config.js
@@ -1,29 +1,27 @@
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
-    if (process.env.NODE_ENV !== 'production') {
-      return [
-        {
-          destination: 'https://playentry.org/lib/:path*',
-          source: '/lib/:path*',
-        },
-        {
-          destination: 'https://playentry.org/js/:path*',
-          source: '/js/:path*',
-        },
-        {
-          destination: 'https://playentry.org/font/:path*',
-          source: '/font/:path*',
-        },
-        {
-          destination: 'https://playentry.org/uploads/:path*',
-          source: '/uploads/:path*',
-        },
-        {
-          destination: 'https://playentry.org/img/:path*',
-          source: '/img/:path*',
-        },
-      ];
-    }
+    return [
+      {
+        destination: 'https://playentry.org/lib/:path*',
+        source: '/lib/:path*',
+      },
+      {
+        destination: 'https://playentry.org/js/:path*',
+        source: '/js/:path*',
+      },
+      {
+        destination: 'https://playentry.org/font/:path*',
+        source: '/font/:path*',
+      },
+      {
+        destination: 'https://playentry.org/uploads/:path*',
+        source: '/uploads/:path*',
+      },
+      {
+        destination: 'https://playentry.org/img/:path*',
+        source: '/img/:path*',
+      },
+    ];
   },
 };
